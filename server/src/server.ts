ffb9067aee0881431a5e5e1719698f41f4df026b
Narayan <choudharynarayan041@gmail.com>
2024-09-09 23:50:36 +0530

    Web Socket Setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,26 @@
-console.log("server in running")
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io'
+import mongoose from 'mongoose';
+
+const app = express();
+const http = createServer(app);
+const io = new Server(http);
+
+app.get('/', (req, res) => {
+    res.send('API is up');
+});
+
+io.on('connection', (socket) => {
+    console.log("socket connected");
+});
+
+mongoose.connect('mongodb://localhost:27017/eltrello').then(() => {
+    console.log("database connected");
+    http.listen(4001, () => {
+        console.log("server in running");
+    });
+});
+
+
+
